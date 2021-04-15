7023c805b88c39b63996987bcfe94b6be4523d0a
a19jimsa <a19jimsa@student.his.se>
2021-04-15 09:50:17 +0200

    Removed line

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -13,7 +13,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setContentView(R.layout.activity_main);
         intent = new Intent(MainActivity.this, SecondActivity.class);
         Button button = (Button) findViewById(R.id.mainButton);
         button.setOnClickListener(new View.OnClickListener() {
